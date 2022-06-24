c7e88b324cc7f0bd6a6bfaf48a02289665265173
ＫＩＮＧメＲØＮＩＮ <king.ronin04@gmail.com>
2022-06-24 09:15:49 +0530

    Update prime_factor.cpp

--- naive/prime_factor.cpp
+++ naive/prime_factor.cpp
@@ -8,21 +8,35 @@ public:
     {
         if (n == 1)
             return false;
+
         if (n == 2 || n == 3)
             return true;
+
         if (n % 2 == 0 || n % 3 == 0)
             return false;
+
         for (int i = 5; i * i <= n; i += 6)
         {
-            if(n%i==0 || n%(i+2)==0)
+            if (n % i == 0 || n % (i + 2) == 0)
                 return false;
         }
         return true;
     }
 
-    int prime_fact(int num)
+    void prime_fact(int num)
     {
-        
+        for (int i = 2; i < num; i++)
+        {
+            if (prime(i))
+            {
+                int x = i;
+                while (num % x == 0)
+                {
+                    cout << i<<" ";
+                    x = x * i;
+                }
+            }
+        }
     }
 };
 
@@ -35,7 +49,7 @@ int main()
         int num;
         cin >> num;
         study ob;
-        cout << ob.prime_fact(num);
+        ob.prime_fact(num);
         cout << "\n";
     }
     return 0;
